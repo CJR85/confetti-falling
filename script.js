7f5236ba6a8dd27aa9088f1b4dc88c2d361bb6dd
Calvin Roberts <roberts.calvin85@gmail.com>
2021-07-13 23:43:50 +0100

    Add function to close button on popup.

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 let btn = document.querySelector('.btn'),
-  popup = document.querySelector('.popup');
+  popup = document.querySelector('.popup'),
+  close = document.querySelector('.close');
 btn.onclick = function () {
   popup.classList.add('active');
+};
+close.onclick = function () {
+  popup.classList.remove('active');
 };
