f43005698c2872c007ea39e9f9c3bedf22e3433a
Calvin Roberts <roberts.calvin85@gmail.com>
2021-07-14 00:02:06 +0100

    Add function to activate confetti.

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let btn = document.querySelector('.btn'),
   popup = document.querySelector('.popup'),
-  close = document.querySelector('.close');
+  close = document.querySelector('.close'),
+  confetti = document.querySelector('#my-canvas');
 btn.onclick = function () {
   popup.classList.add('active');
+  confetti.classList.add('active');
 };
 close.onclick = function () {
   popup.classList.remove('active');
