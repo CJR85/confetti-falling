997db28675dd9b1f12176e90097be371ddec4bdd
Calvin Roberts <roberts.calvin85@gmail.com>
2021-07-14 00:06:20 +0100

    Ammend selector name.

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let btn = document.querySelector('.btn'),
   popup = document.querySelector('.popup'),
   close = document.querySelector('.close'),
-  confetti = document.querySelector('#my-canvas');
+  confe = document.querySelector('#my-canvas');
 btn.onclick = function () {
   popup.classList.add('active');
-  confetti.classList.add('active');
+  confe.classList.add('active');
 };
 close.onclick = function () {
   popup.classList.remove('active');
